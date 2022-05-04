dcdeb506c676eca48da487ef607dd0c242217468
Ovidiu Anton Anton OVIDIUs <ovidiugabriel123456789@outlook.com>
2022-05-04 09:52:55 +0300

    laborator2

--- Algoritmica.Laborator2/Program.cs
+++ Algoritmica.Laborator2/Program.cs
@@ -4,9 +4,78 @@ namespace Algoritmica.Laborator2
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            
-        }
+        //static void Main(string[] args)
+        //{
+        //    //Ex 1: Scrieti un program care va afisa factorialul unui numar n, n fiind citit
+        //    //de la tastatura
+        //    int numar;
+
+        //    Console.WriteLine("Tasteaza un numar dorit! ");
+        //    numar = int.Parse(Console.ReadLine());
+
+        //    if (numar <= 0)
+        //    {
+        //        Console.WriteLine("Introduce-ti un numar valid! ");
+        //        return;
+        //    }
+        //    long factorial = 1;
+        //    for (int i = 1; i <= numar; i++)
+        //    {
+        //        factorial *= i;
+        //    }
+        //    Console.WriteLine(factorial);
+
+
+        //}
+
+        //Ex 2 Să se scrie un program care citeşte de la tastatură un şir de n numere
+        //naturale şi determină media aritmetică a celor pare, n fiind citit de la
+        //tastatra
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Introduceti n numere dorite de la tastatura! ");
+        //    int n = int.Parse(Console.ReadLine());
+
+        //    if(n <= 0)
+        //    {
+        //        Console.WriteLine("Introduceti un numar valid! ");
+        //        return;
+        //    }
+
+        //    long suma = 0;
+        //    int numarPar = 0;
+
+        //    for(int i = 1; i <= n; i++)
+        //    {
+        //        Console.WriteLine("Introdu numarul! ");
+        //        int numar = int.Parse(Console.ReadLine());
+
+        //        if(numar % 2 == 0)
+        //        {
+        //            suma += numar;
+        //            numarPar++;
+        //        }
+        //    }
+
+        //    double mediaAritmetica = 0;
+
+        //    if(numarPar != 0)
+        //    {
+        //        mediaAritmetica = (double)suma / numarPar;
+        //    }
+
+        //    Console.WriteLine("Media aritmetica este : " + mediaAritmetica);
+        //}
+
+
+
     }
+
+    //• Ex 3: Scrieti un program care va determina daca un numar este sau nu
+    //palindrom.
+
+
+
 }
+
